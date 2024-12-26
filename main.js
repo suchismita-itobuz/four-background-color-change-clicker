eea5572781bf248df90581b7b8bc0ef3ed7a2f29
Suchismita Singha Roy <suchismita@itobuz.com>
2024-12-26 18:36:22 +0530

    task done

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function aqua() {
+    document.body.style.backgroundColor = "aqua"
+}
+
+function lightblue() {
+    document.body.style.backgroundColor = "lightblue"
+}
+function coral() {
+    document.body.style.backgroundColor = "coral"
+}
+function grey() {
+    document.body.style.backgroundColor = "grey"
+}
